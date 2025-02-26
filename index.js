e23e6df92796c3198afb20e6f3a20d725647b7a3
Marcelo Aug <m45marceloaugusto@gmail.com>
2025-02-26 18:25:08 -0300

    "feat: add routeto create a new task"

--- index.js
+++ index.js
@@ -2,18 +2,36 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const connectToDatabase = require("./src/database/mongoose.database.js");
+const TaskModel = require('./src/models/task.model.js');
 
 dotenv.config();
 
 const server = express();
+server.use(express.json()) /// Fala para o expres que a gente vai receber json na body das requisições
 const PORT = 8000;
 
 connectToDatabase();
 
-server.get('/', (req, res)=>{
-    const tasks = [{ description: "Estudar"}]
-    res.status(200).send(tasks)
-})
+server.get('/tasks', async (req, res)=>{
+    try{
+        const tasks = await TaskModel.find({})
+        res.status(200).send(tasks)
+    } catch(erro){
+        res.status(500).send(erro.message)
+    }
+});
+
+server.post("/tasks", async (req, res) =>{
+    try {
+        const newTask = new TaskModel(req.body);
+
+        await newTask.save();
+
+        res.status(201).send(newTask);
+    } catch (error) {
+        res.status(500).send(error.mensage)
+    }
+} )
 
 server.listen(PORT,()=>{
     console.log(`Servidor rodando na porta ${PORT}`)
